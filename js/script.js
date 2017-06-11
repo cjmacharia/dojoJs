5f161cdacdbcf627391e9e105eecf2f16f4e0575
collins <collomash4@gmail.com>
2017-06-11 20:53:16 +0300

    updating the added friend to the list of existing friends

--- js/script.js
+++ js/script.js
@@ -15,6 +15,20 @@ function addActivity() {
 function addFriend() {
     var li = document.createElement("li");
     var add = document.getElementById("friends").value;
+    var tableRow = document.createElement("tr");
+    var tableData = document.createElement("td");
+    var tableData_check = document.createElement("td");
+
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    tableRow.append(tableData);
+    tableRow.append(tableData_check);
+
+
+    tableData.append(add);
+    tableData_check.append(checkbox);
+
     li.append(add);
 
     if (add == "") {
@@ -22,5 +36,6 @@ function addFriend() {
     }
     else {
         document.getElementById("myFriends").append(li);
+        document.getElementById("table_friends").append(tableRow);
     }
 }
